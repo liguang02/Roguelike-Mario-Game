0ff88ef2baf560e41e78e397050c4988a6260848
esea0003 <esea0003@student.monash.edu>
2022-05-01 18:30:01 +0800

    Set monologues arraylist and SpeakAction function

--- src/game/actors/Toad.java
+++ src/game/actors/Toad.java
@@ -10,13 +10,14 @@ import game.actions.PurchaseAction;
 import game.items.PowerStar;
 import game.items.SuperMushroom;
 import game.items.Wrench;
+import game.speaking.SpeakAction;
 
 public class Toad extends Actor {
     public Toad(){
         super("Toad", 'o',50);
     }
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        return new DoNothingAction();
+        return new SpeakAction();
     }
 
     public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
@@ -24,6 +25,7 @@ public class Toad extends Actor {
         actions.add(new PurchaseAction(new Wrench()));
         actions.add(new PurchaseAction(new SuperMushroom()));
         actions.add(new PurchaseAction(new PowerStar()));
+        actions.add(new SpeakAction());
         return actions;
     }
 }
--- src/game/speaking/Monologues.java
+++ src/game/speaking/Monologues.java
@@ -2,12 +2,15 @@ package game.speaking;
 
 import java.util.ArrayList;
 
-public class Monologues {
-    public static void main(String[] args) {
-        ArrayList<String> monologues = new ArrayList<>();
-        monologues.add("You might need a wrench to smash Koopa's hard shells.");
-        monologues.add("You better get back to finding the Power Stars.");
-        monologues.add("The Princess is depending on you! You are our only hope.");
-        monologues.add("Being imprisoned in these walls can drive a fungus crazy :(");
+public class Monologues extends SpeakAction {
+
+    public ArrayList Monologues(){
+        ArrayList<String> allMonologues = new ArrayList<>();
+        allMonologues.add("You might need a wrench to smash Koopa's hard shells.");
+        allMonologues.add("You better get back to finding the Power Stars.");
+        allMonologues.add("The Princess is depending on you! You are our only hope.");
+        allMonologues.add("Being imprisoned in these walls can drive a fungus crazy :(");
+        return allMonologues;
     }
+
 }
