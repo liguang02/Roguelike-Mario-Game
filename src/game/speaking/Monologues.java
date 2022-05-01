01e8bd7f9633f3f128cf1261e789d47c84efea39
esea0003 <esea0003@student.monash.edu>
2022-05-01 17:49:29 +0800

    Added Monologues

---
+++ src/game/speaking/Monologues.java
@@ -0,0 +1,13 @@
+package game.speaking;
+
+import java.util.ArrayList;
+
+public class Monologues {
+    public static void main(String[] args) {
+        ArrayList<String> monologues = new ArrayList<>();
+        monologues.add("You might need a wrench to smash Koopa's hard shells.");
+        monologues.add("You better get back to finding the Power Stars.");
+        monologues.add("The Princess is depending on you! You are our only hope.");
+        monologues.add("Being imprisoned in these walls can drive a fungus crazy :(");
+    }
+}
