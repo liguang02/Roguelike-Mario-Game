8f19fdd5e7e19d944b76931b020b77287440b497
LiGuangChai <lcha0068@student.monash.edu>
2022-05-02 15:04:07 +0800

    Updated the application class to be back to the default version and added javadocs to it

--- src/game/Application.java
+++ src/game/Application.java
@@ -8,8 +8,6 @@ import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
-import game.actors.Goomba;
-import game.actors.Koopa;
 import game.actors.Player;
 import game.actors.Toad;
 import game.grounds.Dirt;
@@ -18,18 +16,19 @@ import game.grounds.Wall;
 import game.grounds.trees.Mature;
 import game.grounds.trees.Sapling;
 import game.grounds.trees.Sprout;
-import game.grounds.trees.Tree;
-import game.items.Coin;
-import game.items.PowerStar;
-import game.items.SuperMushroom;
-import game.items.Wrench;
 
 /**
  * The main class for the Mario World game.
- *
+ * @version 1.1.0
+ * @author sthi0011, lcha0068, esea0003
  */
 public class Application {
-
+	/**
+	 * The driver class of the game. It will create game map and add it into the world.
+	 * Where player can be added into the world and other npcs such as toad can be added into the game map.
+	 * It will run the game by calling world.run()
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 			World world = new World(new Display());
@@ -61,20 +60,9 @@ public class Application {
 			world.addGameMap(gameMap);
 
 			Actor mario = new Player("mario", 'm', 100);
-			world.addPlayer(mario, gameMap.at(44, 10));
-			mario.addItemToInventory(new PowerStar());
-			mario.addItemToInventory(new Wrench());
-			mario.addItemToInventory(new SuperMushroom());
+			world.addPlayer(mario, gameMap.at(42, 10));
+			gameMap.at(45,10).addActor(new Toad());
 
-			// FIXME: the Goomba should be generated from the Tree
-			gameMap.at(2, 1).addActor(new Koopa());
-			gameMap.at(1,2).addActor(new Goomba());
-
-			gameMap.at(42, 10).addItem(new Coin(100000));
-			gameMap.at(41, 10).addItem(new PowerStar());
-			gameMap.at(43, 10).addItem(new PowerStar());
-			gameMap.at(44, 10).addItem(new SuperMushroom());
-			gameMap.at(45, 10).addActor(new Toad());
 
 			world.run();
 
