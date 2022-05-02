4ba4671db38b986cf0a2b1da795cd714dad51084
esea0003 <esea0003@student.monash.edu>
2022-05-02 11:39:43 +0800

    Changed arraylist to hashmap to store monologues

--- src/game/Monologues.java
+++ src/game/Monologues.java
@@ -1,21 +1,23 @@
 package game;
 
 import game.actors.Toad;
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Monologues extends Toad {
 
-    public ArrayList AllMonologues(){
-        ArrayList<String> allMonologues = new ArrayList<>();
-        allMonologues.add("You might need a wrench to smash Koopa's hard shells.");
-        allMonologues.add("You better get back to finding the Power Stars.");
-        allMonologues.add("The Princess is depending on you! You are our only hope.");
-        allMonologues.add("Being imprisoned in these walls can drive a fungus crazy :(");
+    public HashMap AllMonologues(){
+        HashMap<Integer, String> allMonologues = new HashMap<>();
+        allMonologues.put(0, "You might need a wrench to smash Koopa's hard shells.");
+        allMonologues.put(1, "You better get back to finding the Power Stars.");
+        allMonologues.put(2, "The Princess is depending on you! You are our only hope.");
+        allMonologues.put(3, "Being imprisoned in these walls can drive a fungus crazy :(");
         return allMonologues;
     }
 
-    public ArrayList<String> getAllMonologues() {
+    public HashMap<Integer, String> getAllMonologues() {
         return AllMonologues();
     }
 
--- src/game/actions/SpeakAction.java
+++ src/game/actions/SpeakAction.java
@@ -9,43 +9,32 @@ import game.actors.Status;
 import game.items.Wrench;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SpeakAction extends Action {
     private Monologues monologues;
+    private int lower;
 
     @Override
     public String execute(Actor actor, GameMap map) {
         monologues = new Monologues();
-        ArrayList<String> monologueList = monologues.getAllMonologues();
+        HashMap<Integer, String> monologueList = monologues.getAllMonologues();
         for (Item item : actor.getInventory()) {
             if (item.getClass() == Wrench.class) {
-                for (int i = 0; i < monologueList.size(); i++){
-                    if(monologueList.get(i).equals("You might need a wrench to smash Koopa's hard shells.")){
-                        monologueList.remove(i);
-                    }
-
+                monologueList.remove(0);
                 }
             }
-        }
-
-        if (actor.hasCapability(Status.CONSUMER_STAR)) {
-            for (int i = 0; i < monologueList.size(); i++){
-                if(monologueList.get(i).equals("You better get back to finding the Power Stars.")){
-                    monologueList.remove(i);
-                }
-            }
-        }
 
         if (actor.hasCapability(Status.INVINCIBLE)) {
             for (int i = 0; i < monologueList.size(); i++){
-                if(monologueList.get(i).equals("You better get back to finding the Power Stars.")){
-                    monologueList.remove(i);
-                }
+                monologueList.remove(1);
             }
         }
 
-        int randomNum = ThreadLocalRandom.current().nextInt(0, monologueList.size());
+        Object randomNum = monologueList.keySet().toArray()[new Random().nextInt(monologueList.keySet().toArray().length)];
         String item = monologueList.get(randomNum);
         return item;
     }
