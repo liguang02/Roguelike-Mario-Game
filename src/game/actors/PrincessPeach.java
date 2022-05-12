195c5aa64c534bdd27383370d4152560d2a92631
esea0003 <esea0003@student.monash.edu>
2022-05-13 00:10:23 +0800

    Added Bowser enemy class

--- src/game/actors/PrincessPeach.java
+++ src/game/actors/PrincessPeach.java
@@ -16,7 +16,7 @@ public class PrincessPeach extends Allies{
      */
     private final Monologue monologues = new Monologue();
     public PrincessPeach() {
-        super("Princess Peach", '♕',50);
+        super("Princess Peach", 'P',50);
         monologues.addMonologues(Status.KEY, "Thank you for saving me!");
         monologues.addMonologues(Status.GENERIC, "Dear Mario, I'll be waiting for you...");
         monologues.addMonologues(Status.GENERIC, "Never gonna give you up!");
