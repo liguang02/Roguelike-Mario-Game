300d0add48f61b066cf2c61e3412356e3910bb8e
LiGuangChai <lcha0068@student.monash.edu>
2022-05-15 13:25:11 +0800

    updated lava class

--- src/game/grounds/Lava.java
+++ src/game/grounds/Lava.java
@@ -1,10 +1,16 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.GroundInflictAction;
 import game.utilities.Status;
 
 public class Lava extends Ground {
+    private String name;
+    private int damage;
     /**
      * Constructor for Lava ground class
      *
@@ -12,6 +18,8 @@ public class Lava extends Ground {
     public Lava() {
         super('L');
         this.addCapability(Status.FIRE);
+        name =  "Lava";
+        damage = 15;
     }
 
     /**
@@ -22,5 +30,22 @@ public class Lava extends Ground {
     @Override
     public boolean canActorEnter(Actor actor) {
         return !actor.hasCapability(Status.ENEMY);
+    }
+
+    /**
+     * A toString method that will return its name (lava)
+     * @return name of this instance
+     */
+    public String toString(){
+        return name;
+    }
+
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        ActionList actionList = new ActionList();
+        if (!actor.hasCapability(Status.ENEMY)){
+            actionList.add(new GroundInflictAction(Status.FIRE, damage));
+        }
+        return actionList;
     }
 }
